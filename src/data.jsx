1d2e91954c6f3ae40a9fe6777cd020a133c89a39
Dhiyaulhaq Aslam <dhias313@gmail.com>
2025-09-20 14:03:28 +0800

    data cathegorize

--- src/data.jsx
+++ src/data.jsx
@@ -1,14 +1,41 @@
-export const classicBands = [
-  { id: '1', name: 'Linkin Park' },
-  { id: '2', name: 'Aerosmith' },
-  { id: '3', name: 'Green Day' },
-  { id: '4', name: 'Muse' },
-  { id: '5', name: 'Queen' },
-  { id: '6', name: 'Black Sabbath' },
-  { id: '7', name: 'CCR' },
-  { id: '8', name: 'RHCP' },
-  { id: '9', name: 'MLTR' },
-  { id: '10', name: 'Bon Jovi' },
-  { id: '11', name: 'GNR' },
-  { id: '12', name: 'Nirvana' },
-];
+export const classicData = {
+   band: [
+      { id: "1", name: "Linkin Park" },
+      { id: "2", name: "Aerosmith" },
+      { id: "3", name: "Green Day" },
+      { id: "4", name: "Muse" },
+      { id: "5", name: "Queen" },
+      { id: "6", name: "Black Sabbath" },
+      { id: "7", name: "CCR" },
+      { id: "8", name: "RHCP" },
+      { id: "9", name: "MLTR" },
+      { id: "10", name: "Bon Jovi" },
+      { id: "11", name: "GNR" },
+      { id: "12", name: "Nirvana" },
+   ],
+   music: {
+      rock: [
+         { id: "m1", name: "Bohemian Rhapsody" },
+         { id: "m2", name: "Stairway to Heaven" },
+      ],
+      "heavy metal": [
+         { id: "m3", name: "Enter Sandman" },
+         { id: "m4", name: "Fear of the Dark" },
+      ],
+      "thrash metal": [
+         { id: "m5", name: "Master of Puppets" },
+         { id: "m6", name: "Peace Sells" },
+      ],
+      "death metal": [
+        { id: "m7", name: "Hammer Smashed Face" }
+      ],
+      "black metal": [{ id: "m8", name: "Freezing Moon" }],
+      pop: [{ id: "m9", name: "Shape of You" }],
+      punk: [{ id: "m10", name: "American Idiot" }],
+      jazz: [{ id: "m11", name: "Take Five" }],
+      "r&b": [{ id: "m12", name: "Blinding Lights" }],
+      country: [{ id: "m13", name: "Take Me Home, Country Roads" }],
+      "hip hop": [{ id: "m14", name: "Lose Yourself" }],
+      kpop: [{ id: "m15", name: "Dynamite" }],
+   },
+};
