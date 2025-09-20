e50681cd8b5c83cd76bda96fcb6b3bf22f4d0172
Dhiyaulhaq Aslam <dhias313@gmail.com>
2025-09-20 11:52:39 +0800

    data.jsx

---
+++ src/data.jsx
@@ -0,0 +1,7 @@
+export const classicBands = [
+  { id: '1', name: 'Linkin Park' },
+  { id: '2', name: 'Coldplay' },
+  { id: '3', name: 'Green Day' },
+  { id: '4', name: 'Muse' },
+  { id: '5', name: 'Paramore' },
+];
