9808d47781a2b91da002ae1b566ed708f77651da
Dhiyaulhaq Aslam <dhias313@gmail.com>
2025-09-20 12:37:16 +0800

    adding new data

--- src/data.jsx
+++ src/data.jsx
@@ -1,7 +1,14 @@
 export const classicBands = [
   { id: '1', name: 'Linkin Park' },
-  { id: '2', name: 'Coldplay' },
+  { id: '2', name: 'Aerosmith' },
   { id: '3', name: 'Green Day' },
   { id: '4', name: 'Muse' },
-  { id: '5', name: 'Paramore' },
+  { id: '5', name: 'Queen' },
+  { id: '6', name: 'Black Sabbath' },
+  { id: '7', name: 'CCR' },
+  { id: '8', name: 'RHCP' },
+  { id: '9', name: 'MLTR' },
+  { id: '10', name: 'Bon Jovi' },
+  { id: '11', name: 'GNR' },
+  { id: '12', name: 'Nirvana' },
 ];
